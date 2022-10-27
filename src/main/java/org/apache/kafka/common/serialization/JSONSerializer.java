cb11753d453c0dda7e4eb2bd50e5db34944cbf1e
Hugh <15069071152@139.com>
2022-10-27 17:39:07 +0800

    .

---
+++ src/main/java/org/apache/kafka/common/serialization/JSONSerializer.java
@@ -0,0 +1,29 @@
+package org.apache.kafka.common.serialization;
+
+import java.util.Map;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+public class JSONSerializer implements Serializer<JSONObject> {
+	private String encoding = "UTF8";
+
+	public void configure(Map<String, ?> configs, boolean isKey) {
+		String propertyName = isKey ? "key.serializer.encoding" : "value.serializer.encoding";
+		Object encodingValue = configs.get(propertyName);
+		if (encodingValue == null)
+			encodingValue = configs.get("serializer.encoding");
+		if (encodingValue != null && encodingValue instanceof String)
+			encoding = (String) encodingValue;
+	}
+
+	public byte[] serialize(String topic, JSONObject data) {
+		if (data == null)
+			return null;
+		return JSON.toJSONBytes(data);
+	}
+
+	public void close() {
+	}
+
+}
